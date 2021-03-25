0d9ad9bdabfdeb34bafd01ccc268e13db2d9e076
edutsi <nilson.upis@gmail.com>
2021-03-24 22:35:19 -0300

    Curso Spring Boot: Login do Usuario Parte 1

--- src/main/java/br/com/academy/controllers/HomeController.java
+++ src/main/java/br/com/academy/controllers/HomeController.java
@@ -4,15 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.academy.model.Aluno;
+
 @Controller//informa que essa é a classe controller
 public class HomeController {
 
-	@GetMapping("/")//redireciona para a pagina principal
+	@GetMapping("/index")//redireciona para a pagina principal
 	public ModelAndView index() {
 		
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("home/index");
-		mv.addObject("msg", "Mensagem vinda diretamente do controller");
+		mv.addObject("aluno", new Aluno());
 		return mv;
 	}
 	
